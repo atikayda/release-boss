b98e93c4fb61f1fd3ac9620e8275040736b324a3
GitHub Action <action@github.com>
2025-04-23 13:49:20 +0000

    chore: update dist files

--- dist/index.js
+++ dist/index.js
@@ -66226,15 +66226,15 @@ module.exports = {
  * It gets processed during releases to update the version number
  */
 
-const VERSION = '1.4.0';
-const VERSION_WITH_V = 'v1.4.0';
+const VERSION = '1.4.1';
+const VERSION_WITH_V = 'v1.4.1';
 
 module.exports = {
   VERSION,
   VERSION_WITH_V,
   MAJOR: '1',
   MINOR: '4',
-  PATCH: '0'
+  PATCH: '1'
 };
 
 
